efa6e5579bf69a79f238b7efbcaf6a88652000f3
Andrew Tran <andrew_tran@takarabio.com>
2024-07-24 11:31:37 -0700

    disable obsidian ui features and just use markdown instead

--- lua/custom/plugins/obsidian.lua
+++ lua/custom/plugins/obsidian.lua
@@ -1,29 +1,33 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
   lazy = false,
   -- ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
   event = {
     -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
     -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-    "BufReadPre " .. vim.fn.expand "~" .. "/Documents/Obsidian/work/**.md",
-    "BufNewFile " .. vim.fn.expand "~" .. "/Documents/Obsidian/work/**.md",
+    'BufReadPre '
+      .. vim.fn.expand '~'
+      .. '/Documents/Obsidian/work/**.md',
+    'BufNewFile ' .. vim.fn.expand '~' .. '/Documents/Obsidian/work/**.md',
   },
   dependencies = {
     -- Required.
-    "nvim-lua/plenary.nvim",
+    'nvim-lua/plenary.nvim',
 
     -- see below for full list of optional dependencies 👇
   },
   opts = {
     workspaces = {
       {
-        name = "work",
-        path = "~/Documents/Obsidian/work",
+        name = 'work',
+        path = '~/Documents/Obsidian/work',
       },
     },
-
-    -- see below for full list of options 👇
+    ui = {
+      enable = false,
+    },
   },
 }
+
