d97c55cfbc7b9282d89699e8aba049f6ec4657d3
Andrew Tran <andrew_tran@takarabio.com>
2025-03-09 20:47:38 -0700

    fix init

--- init.lua
+++ init.lua
@@ -971,12 +971,6 @@ require('lazy').setup({
     lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
