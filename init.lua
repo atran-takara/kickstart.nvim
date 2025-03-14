0697398ea4a30c87f3a021eff6f8fd9c0365db71
Andrew Tran <andrew_tran@takarabio.com>
2025-03-14 15:01:37 -0700

    fix autoformatter

--- init.lua
+++ init.lua
@@ -814,11 +814,16 @@ require('lazy').setup({
     },
     opts = {
       notify_on_error = false,
-      format_on_save = function(bufnr)
+      format_after_save = function(bufnr)
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true, cs = true, py = true }
+
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          return
+        end
+
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           lsp_format_opt = 'never'
